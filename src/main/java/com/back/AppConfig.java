810529203de91e7d0bbe0200fe3995aa8b9a38a9
gandaraipower <jjangjongsoo@naver.com>
2025-08-28 14:39:02 +0900

    4강

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,14 +1,13 @@
 package com.back;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public PersonService personService(){
-        System.out.println("AppConfig.personService 호출됨!!");
-        return new PersonService();
-    }
+//    @Bean
+//    public PersonService personService(){
+//        System.out.println("AppConfig.personService 호출됨!!");
+//        return new PersonService();
+//    }
 }
