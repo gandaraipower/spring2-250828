a7ca8c3ec753a469a9303e9d352aa536a474d021
gandaraipower <jjangjongsoo@naver.com>
2025-08-28 14:18:26 +0900

    2강

---
+++ src/main/java/com/back/PersonController.java
@@ -0,0 +1,15 @@
+package com.back;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PersonController {
+
+    private PersonService personService= new PersonService();
+
+    @GetMapping("/people")
+    public void people() {
+        System.out.println("people");
+    }
+}
