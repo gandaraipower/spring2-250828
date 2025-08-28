354a87ae291183767c2dcc454b9d20cc9c691d0e
gandaraipower <jjangjongsoo@naver.com>
2025-08-28 15:52:02 +0900

    7강

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,35 @@ public class AppConfig {
     public PersonRepository personRepositoryV2() {
         return new PersonRepository(2);
     }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner3(){
+        return (args -> {
+            System.out.println("MyApplicationRunner3.run");
+            work1();
+            work2();
+        });
+    }
+
+    public void work1(){
+        System.out.println("work1");
+    }
+
+    public void work2(){
+        System.out.println("work2");
+    }
+
+//    @Bean
+//    public MyApplicationRunner myApplicationRunner1(){
+//        System.out.println("AppConfig.myApplicationRunner1");
+//        return new MyApplicationRunner(1);
+//    }
+//
+//    @Bean
+//    public MyApplicationRunner myApplicationRunner2(){
+//        System.out.println("AppConfig.myApplicationRunner2");
+//        return new MyApplicationRunner(2);
+//    }
 
 
 }
