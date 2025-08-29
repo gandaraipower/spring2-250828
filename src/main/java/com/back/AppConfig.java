b3ce6f0d6e8da7ec5f28237b66b28d4ac6682400
gandaraipower <jjangjongsoo@naver.com>
2025-08-29 10:32:48 +0900

    10강

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,51 +1,35 @@
 package com.back;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 public class AppConfig {
 
+    @Autowired
+    @Lazy
+    private AppConfig self;
 
     @Bean
-    public PersonRepository personRepository() {
-        return new PersonRepository(1);
+    public ApplicationRunner myApplicationRunner3() {
+        return args -> {
+            self.work1();
+            self.work2();
+        };
     }
 
-    @Bean
-    public PersonRepository personRepositoryV2() {
-        return new PersonRepository(2);
-    }
-
-    @Bean
-    public ApplicationRunner myApplicationRunner3(){
-        return (args -> {
-            System.out.println("MyApplicationRunner3.run");
-            work1();
-            work2();
-        });
-    }
-
-    public void work1(){
+    @Transactional
+    public void work1() {
         System.out.println("work1");
     }
 
-    public void work2(){
+    @Transactional
+    public void work2() {
         System.out.println("work2");
     }
-
-//    @Bean
-//    public MyApplicationRunner myApplicationRunner1(){
-//        System.out.println("AppConfig.myApplicationRunner1");
-//        return new MyApplicationRunner(1);
-//    }
-//
-//    @Bean
-//    public MyApplicationRunner myApplicationRunner2(){
-//        System.out.println("AppConfig.myApplicationRunner2");
-//        return new MyApplicationRunner(2);
-//    }
-
 
 }
