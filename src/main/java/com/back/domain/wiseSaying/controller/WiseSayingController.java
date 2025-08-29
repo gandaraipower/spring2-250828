73094d26f98d285a535218b9058ae0f505815ba2
gandaraipower <jjangjongsoo@naver.com>
2025-08-29 15:14:26 +0900

    19강

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,6 +4,7 @@ import com.back.domain.wiseSaying.entity.WiseSaying;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -14,8 +15,15 @@ import java.util.stream.Collectors;
 @Controller
 public class WiseSayingController {
 
-    private List<WiseSaying> wiseSayingList = new ArrayList<>();
-    private int lastId = 0;
+    private List<WiseSaying> wiseSayingList = new ArrayList<>() {{
+        add(new WiseSaying(1, "명언1", "작가1"));
+        add(new WiseSaying(2, "명언2", "작가2"));
+        add(new WiseSaying(3, "명언3", "작가3"));
+        add(new WiseSaying(4, "명언4", "작가4"));
+        add(new WiseSaying(5, "명언5", "작가5"));
+    }};
+
+    private int lastId = 5;
 
     @GetMapping("/wiseSaying/write")
     @ResponseBody
@@ -57,19 +65,39 @@ public class WiseSayingController {
     @ResponseBody
     public String delete(
             @PathVariable int id
-    ){
+    ) {
 
-        Optional<WiseSaying> wiseSaying=wiseSayingList.stream()
-                .filter(w->w.getId() ==id )
+        WiseSaying wiseSaying = findById(id);
+        wiseSayingList.remove(wiseSaying);
+
+        return "%d번 명언이 삭제되었습니다".formatted(id);
+    }
+
+    @GetMapping("/wiseSaying/modify/{id}")
+    @ResponseBody
+    public String modify(
+            @PathVariable int id,
+            @RequestParam(defaultValue = "기본값") String content,
+            @RequestParam(defaultValue = "기본값") String author
+    ) {
+
+        WiseSaying wiseSaying = findById(id);
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
+
+        return "%d번 명언이 수정되었습니다.".formatted(wiseSaying.getId());
+    }
+
+    private WiseSaying findById(int id) {
+        Optional<WiseSaying> wiseSaying = wiseSayingList.stream()
+                .filter(w -> w.getId() == id)
                 .findFirst();
 
         if(wiseSaying.isEmpty()) {
             throw new RuntimeException("%d번 명언은 존재하지 않습니다.".formatted(id));
         }
 
-        wiseSayingList.remove(wiseSaying.get());
-
-        return "%d번 명언이 삭제되었습니다.".formatted(id);
+        return wiseSaying.get();
     }
 
 }
--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -2,11 +2,14 @@ package com.back.domain.wiseSaying.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
+@Setter
+@AllArgsConstructor
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
+
 }
