a2f6160133bf556566f77dc27130fdf277003949
gandaraipower <jjangjongsoo@naver.com>
2025-08-29 15:02:24 +0900

    18강

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -3,10 +3,12 @@ package com.back.domain.wiseSaying.controller;
 import com.back.domain.wiseSaying.entity.WiseSaying;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -49,6 +51,25 @@ public class WiseSayingController {
                 %s
                 </ul>
                 """.formatted(wiseSayings);
+    }
+
+    @GetMapping("/wiseSaying/delete/{id}")
+    @ResponseBody
+    public String delete(
+            @PathVariable int id
+    ){
+
+        Optional<WiseSaying> wiseSaying=wiseSayingList.stream()
+                .filter(w->w.getId() ==id )
+                .findFirst();
+
+        if(wiseSaying.isEmpty()) {
+            throw new RuntimeException("%d번 명언은 존재하지 않습니다.".formatted(id));
+        }
+
+        wiseSayingList.remove(wiseSaying.get());
+
+        return "%d번 명언이 삭제되었습니다.".formatted(id);
     }
 
 }
