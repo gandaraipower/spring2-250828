600ca445f1d4c733bc3077d8cabe69b657a9ca70
gandaraipower <jjangjongsoo@naver.com>
2025-09-01 12:31:07 +0900

    32강,@Transaction을 붙인 후 ãdelete 쿼리

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -5,6 +5,7 @@ import com.back.domain.wiseSaying.service.WiseSayingService;
 import com.back.standard.MarkdownService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -71,6 +72,7 @@ public class WiseSayingController {
 
     @GetMapping("/wiseSayings/delete/{id}")
     @ResponseBody
+    @Transactional
     public String delete(
             @PathVariable int id
     ) {
